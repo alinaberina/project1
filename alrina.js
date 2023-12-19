ba8061093bd533a8b284a66df1016a1d92dae68f
alinaberina <alinaaam044berina@gmail.com>
2023-12-19 22:58:34 +0300

    initial commit

--- alrina.js
+++ alrina.js
@@ -42,3 +42,199 @@ $(window).on("load resize", function() {
     $dropdown.off("mouseenter mouseleave");
   }
 });
+/*timer*/
+const countdownDate = new Date(2024, 0, 1, 0, 0, 0, 0);
+let timerInterval;
+const daysElem = document.getElementById("days"),
+  hoursElem = document.getElementById("hours"),
+  minutesElem = document.getElementById("minutes"),
+  secondsElem = document.getElementById("seconds"),
+  timer = document.getElementById("timer"),
+  content = document.getElementById("content");
+const formatTime = (time, string) => {
+  return time == 1 ? `${time} ${string}` : `${time} ${string}s`;
+};
+const startCountdown = () => {
+  const now = new Date().getTime();
+  const countdown = new Date(countdownDate).getTime();
+  const difference = (countdown - now) / 1000;
+  if (difference < 1) {
+    endCountdown();
+  }
+  let days = Math.floor(difference / (60 * 60 * 24));
+  let hours = Math.floor((difference % (60 * 60 * 24)) / (60 * 60));
+  let minutes = Math.floor((difference % (60 * 60)) / 60);
+  let seconds = Math.floor(difference % 60);
+  daysElem.innerHTML = formatTime(days, "day");
+  hoursElem.innerHTML = formatTime(hours, "hour");
+  minutesElem.innerHTML = formatTime(minutes, "minute");
+  secondsElem.innerHTML = formatTime(seconds, "second");
+};
+const endCountdown = () => {
+  clearInterval(timerInterval);
+  timer.remove();
+  content.classList.add("visible");
+};
+window.addEventListener("load", () => {
+  startCountdown();
+  timerInterval = setInterval(startCountdown, 1000);
+});
+/*snow*/
+const snowContainer = document.getElementById("snow-container");
+const snowContent = ['&#10052', '&#10053', '&#10054']
+const random = (num) => {
+  return Math.floor(Math.random() * num);
+}
+const getRandomStyles = () => {
+  const top = random(100);
+  const left = random(100);
+  const dur = random(10) + 10;
+  const size = random(25) + 25;
+  return `
+    top: -${top}%;
+    left: ${left}%;
+    font-size: ${size}px;
+    animation-duration: ${dur}s;
+  `;
+}
+const createSnow = (num) => {
+  for (var i = num; i > 0; i--) {
+    var snow = document.createElement("div");
+    snow.className = "snow";
+    snow.style.cssText = getRandomStyles();
+    snow.innerHTML = snowContent[random(3)]
+    snowContainer.append(snow);
+  }
+}
+window.addEventListener("load", () => {
+  createSnow(30)
+});
+(function () {
+  // VARIABLES
+  const timeline = document.querySelector(".timeline ol"),
+    elH = document.querySelectorAll(".timeline li > div"),
+    arrows = document.querySelectorAll(".timeline .arrows .arrow"),
+    arrowPrev = document.querySelector(".timeline .arrows .arrow__prev"),
+    arrowNext = document.querySelector(".timeline .arrows .arrow__next"),
+    firstItem = document.querySelector(".timeline li:first-child"),
+    lastItem = document.querySelector(".timeline li:last-child"),
+    xScrolling = 280,
+    disabledClass = "disabled";
+  // START
+  window.addEventListener("load", init);
+  function init() {
+    setEqualHeights(elH);
+    animateTl(xScrolling, arrows, timeline);
+    setSwipeFn(timeline, arrowPrev, arrowNext);
+    setKeyboardFn(arrowPrev, arrowNext);
+  }
+  // SET EQUAL HEIGHTS
+  function setEqualHeights(el) {
+    let counter = 0;
+    for (let i = 0; i < el.length; i++) {
+      const singleHeight = el[i].offsetHeight;
+      if (counter < singleHeight) {
+        counter = singleHeight;
+      }
+    }
+    for (let i = 0; i < el.length; i++) {
+      el[i].style.height = `${counter}px`;
+    }
+  }
+  // CHECK IF AN ELEMENT IS IN VIEWPORT
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  // SET STATE OF PREV/NEXT ARROWS
+  function setBtnState(el, flag = true) {
+    if (flag) {
+      el.classList.add(disabledClass);
+    } else {
+      if (el.classList.contains(disabledClass)) {
+        el.classList.remove(disabledClass);
+      }
+      el.disabled = false;
+    }
+  }
+  // ANIMATE TIMELINE
+  function animateTl(scrolling, el, tl) {
+    let counter = 0;
+    for (let i = 0; i < el.length; i++) {
+      el[i].addEventListener("click", function () {
+        if (!arrowPrev.disabled) {
+          arrowPrev.disabled = true;
+        }
+        if (!arrowNext.disabled) {
+          arrowNext.disabled = true;
+        }
+        const sign = this.classList.contains("arrow__prev") ? "" : "-";
+        if (counter === 0) {
+          tl.style.transform = `translateX(-${scrolling}px)`;
+        } else {
+          const tlStyle = getComputedStyle(tl);
+          // add more browser prefixes if needed here
+          const tlTransform =
+            tlStyle.getPropertyValue("-webkit-transform") ||
+            tlStyle.getPropertyValue("transform");
+          const values =
+            parseInt(tlTransform.split(",")[4]) +
+            parseInt(`${sign}${scrolling}`);
+          tl.style.transform = `translateX(${values}px)`;
+        }
+        setTimeout(() => {
+          isElementInViewport(firstItem)
+            ? setBtnState(arrowPrev)
+            : setBtnState(arrowPrev, false);
+          isElementInViewport(lastItem)
+            ? setBtnState(arrowNext)
+            : setBtnState(arrowNext, false);
+        }, 1100);
+        counter++;
+      });
+    }
+  }
+  // ADD SWIPE SUPPORT FOR TOUCH DEVICES
+  function setSwipeFn(tl, prev, next) {
+    const hammer = new Hammer(tl);
+    hammer.on("swipeleft", () => next.click());
+    hammer.on("swiperight", () => prev.click());
+  }
+  // ADD BASIC KEYBOARD FUNCTIONALITY
+  function setKeyboardFn(prev, next) {
+    document.addEventListener("keydown", (e) => {
+      if (e.which === 37 || e.which === 39) {
+        const timelineOfTop = timeline.offsetTop;
+        const y = window.pageYOffset;
+        if (timelineOfTop !== y) {
+          window.scrollTo(0, timelineOfTop);
+        }
+        if (e.which === 37) {
+          prev.click();
+        } else if (e.which === 39) {
+          next.click();
+        }
+      }
+    });
+  }
+})();
+var dark = false;
+var a = document.body;
+var p = document.getElementById("select")
+function darkLight() {
+	if (!dark) {
+		a.className = "theme-dark";
+		p.innerHTML = "Включить светлую тему";
+	} else {
+		a.className = "theme-light";
+		p.innerHTML = "Включить тёмную тему";
+	}
+	dark = !dark;
+}
